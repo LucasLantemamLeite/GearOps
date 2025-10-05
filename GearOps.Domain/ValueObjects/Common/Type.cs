7c96ffe2895e4c1c87380206ac3fa392a156e99d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-05 15:00:30 -0300

    feat: cria o value object common para o tipo de dispositivo

---
+++ GearOps.Domain/ValueObjects/Common/Type.cs
@@ -0,0 +1,15 @@
+using GearOps.Domain.ValueObjects.Base;
+
+namespace GearOps.Domain.ValueObjects.Common;
+
+public sealed class Type : ValueObject
+{
+    public string Value { get; private set; } = null!;
+
+    public Type(string type)
+    {
+        Value = type;
+    }
+
+    private Type() { }
+}
