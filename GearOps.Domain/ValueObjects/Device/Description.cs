1b07a6fb1cbc0daeb7bcf740dc980ee8d4d43258
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-05 15:59:15 -0300

    feat: cria o value object para pequenas descrições da máquina

---
+++ GearOps.Domain/ValueObjects/Device/Description.cs
@@ -0,0 +1,15 @@
+using GearOps.Domain.ValueObjects.Base;
+
+namespace GearOps.Domain.ValueObjects.Device;
+
+public sealed class Description : ValueObject
+{
+    public string? Value { get; private set; }
+
+    public Description(string? description)
+    {
+        Value = description;
+    }
+
+    private Description() { }
+}
