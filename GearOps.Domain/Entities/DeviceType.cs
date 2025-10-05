9e85c37110f1402e5e6dcc96baa7eb5f06e885d4
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-05 16:02:29 -0300

    feat: cria a entidade de tipos de dispositivos únicos de cada empresa

---
+++ GearOps.Domain/Entities/DeviceType.cs
@@ -0,0 +1,30 @@
+using GearOps.Domain.Entities.Base;
+using Type = GearOps.Domain.ValueObjects.Common.Type;
+using GearOps.Domain.ValueObjects.Common;
+
+namespace GearOps.Domain.Entities;
+
+public sealed class DeviceType : Entity
+{
+    public Image? Image { get; private set; }
+    public Type Type { get; private set; } = null!;
+    public CreatedAt CreatedAt { get; private set; } = new();
+    public CompanyId CompanyId { get; private set; } = null!;
+
+    public DeviceType(string? imageUrl, string type, Guid companyId)
+    {
+        Image = new(imageUrl);
+        Type = new(type);
+        CompanyId = new(companyId);
+    }
+
+    public DeviceType(Guid id, string? imageUrl, string type, DateTime created, Guid companyId) : base(id)
+    {
+        Image = new(imageUrl);
+        Type = new(type);
+        CreatedAt = new(created);
+        CompanyId = new(companyId);
+    }
+
+    private DeviceType() { }
+}
