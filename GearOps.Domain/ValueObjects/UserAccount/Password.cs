01a3e3ee14025c6096ad87433927befbffa00682
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-05 14:56:38 -0300

    feat: cria o value object para senha dos usuários

---
+++ GearOps.Domain/ValueObjects/UserAccount/Password.cs
@@ -0,0 +1,15 @@
+using GearOps.Domain.ValueObjects.Base;
+
+namespace GearOps.Domain.ValueObjects.UserAccount;
+
+public sealed class Password : ValueObject
+{
+    public string Value { get; private set; } = null!;
+
+    public Password(string password)
+    {
+        Value = password;
+    }
+
+    private Password() { }
+}
