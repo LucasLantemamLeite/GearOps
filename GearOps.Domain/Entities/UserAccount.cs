20792c8e978636f463175624893555cf766a6f75
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-05 16:06:18 -0300

    feat: cria a entidade para a conta dos usuários

---
+++ GearOps.Domain/Entities/UserAccount.cs
@@ -0,0 +1,41 @@
+using GearOps.Domain.Entities.Base;
+using GearOps.Domain.ValueObjects.Common;
+using GearOps.Domain.ValueObjects.UserAccount;
+
+namespace GearOps.Domain.Entities;
+
+public sealed class UserAccount : Entity
+{
+    public Name Name { get; private set; } = null!;
+    public Email Email { get; private set; } = null!;
+    public Password Password { get; private set; } = null!;
+    public Phone Phone { get; private set; } = null!;
+    public Role Role { get; private set; } = null!;
+    public CreatedAt CreatedAt { get; private set; } = new();
+    public UpdatedAt UpdatedAt { get; private set; } = new();
+    public CompanyId CompanyId { get; private set; } = null!;
+
+    public UserAccount(string name, string email, string password, string phone, int role, Guid company)
+    {
+        Name = new(name);
+        Email = new(email);
+        Password = new(password);
+        Phone = new(phone);
+        Role = new(role);
+        CompanyId = new(company);
+    }
+
+    public UserAccount(Guid id, string name, string email, string password, string phone, int role, DateTime created, DateTime Updated, Guid company) : base(id)
+    {
+        Name = new(name);
+        Email = new(email);
+        Password = new(password);
+        Phone = new(phone);
+        CreatedAt = new(created);
+        UpdatedAt = new(Updated);
+        Role = new(role);
+        CompanyId = new(company);
+    }
+
+    private UserAccount() { }
+}
