57a71ddf19da2f4272e08fa658f605f5c7c8b1aa
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-07 19:11:22 -0300

    refactor: remove as flag, pelo nível de acesso da aplicação já estar em hierarquia não faz sentido o uso de flags

--- GearOps.Domain/Enums/UserAccount/ERole.cs
+++ GearOps.Domain/Enums/UserAccount/ERole.cs
@@ -1,10 +1,10 @@
 namespace GearOps.Domain.Enums.UserAccount;
 
-[Flags]
 public enum ERole
 {
-    User = 1 << 0, //0001 -> 1
-    Manager = User | 1 << 1, // 0011 -> 3
-    Admin = User | Manager | 1 << 2, // 0111 -> 7
-    Owner = User | Manager | Admin | 1 << 3 // 1111 -> 15
+    User = 1,
+    Manager = 2,
+    Admin = 3,
+    Owner = 4,
+    SuperAdmin = 5
 }
