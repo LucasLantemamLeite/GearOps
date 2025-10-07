03c1dbb71823752f73cd5c4193bf3bd7af9d6648
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-07 20:51:39 -0300

    feat: importa de forma global os enums do domain

--- GearOps.Domain/GlobalUsing.cs
+++ GearOps.Domain/GlobalUsing.cs
@@ -2,3 +2,4 @@ global using GearOps.Domain.ValueObjects.Common;
 global using GearOps.Domain.Entities.Base;
 global using GearOps.Domain.ValueObjects.Base;
 global using GearOps.Domain.Validations.Exceptions;
+global using GearOps.Domain.Enums;
