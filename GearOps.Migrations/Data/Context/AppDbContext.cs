6d0510990534a282d585ffbb41e7597851f1c85f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-10 16:19:18 -0300

    refactor: remove o DbSet da configuração do banco de dados, EF será usado apenas para migrações no banco

--- GearOps.Migrations/Data/Context/AppDbContext.cs
+++ GearOps.Migrations/Data/Context/AppDbContext.cs
@@ -1,16 +1,8 @@
-using GearOps.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-
 namespace GearOps.Migrations.Data.Context;
 
 public sealed class AppDbContext : DbContext
 {
-    public DbSet<Company> Companies { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Device> Devices { get; set; }
-    public DbSet<DeviceType> DeviceTypes { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=GearOpsDb;Username=postgres;Password=Lucas1971!;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 }
